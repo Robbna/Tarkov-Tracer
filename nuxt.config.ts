433b4c33e6df7fe23cc047ad57ce99f91d029360
devs1k0 <devs1ko.dev@gmail.com>
2024-05-28 10:16:58 +0200

    fix: Google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,16 +3,27 @@ export default defineNuxtConfig({
 	devtools: {
 		enabled: true,
 	},
-	runtimeConfig: {
-		public: {
-			gtagId: "G-YTNP4RYFY1",
-		},
-	},
-	plugins: [
-		"~/plugins/gtag.client.js",
-	],
 	modules: ["nuxt-primevue", "@pinia/nuxt", "@nuxtjs/device"],
 	css: ["~/assets/css/main.css"],
+	app: {
+		head: {
+			script: [
+				{
+					innerHTML: `
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+					
+						gtag('config', 'G-YTNP4RYFY1');
+					`,
+				},
+				{
+					src: "https://www.googletagmanager.com/gtag/js?id=G-YTNP4RYFY1",
+					async: true,
+				},
+			],
+		},
+	},
 	// TAILWIND CSS
 	postcss: {
 		plugins: {

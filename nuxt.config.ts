fd262a3edfe4725197dc1d2e5a704d8b59aa28d4
devs1k0 <devs1ko.dev@gmail.com>
2024-05-28 10:32:47 +0200

    refactor: Nuxt Gtag module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,26 +3,11 @@ export default defineNuxtConfig({
 	devtools: {
 		enabled: true,
 	},
-	modules: ["nuxt-primevue", "@pinia/nuxt", "@nuxtjs/device"],
+	modules: ["nuxt-primevue", "@pinia/nuxt", "@nuxtjs/device", "nuxt-gtag"],
 	css: ["~/assets/css/main.css"],
-	app: {
-		head: {
-			script: [
-				{
-					innerHTML: `
-						window.dataLayer = window.dataLayer || [];
-						function gtag(){dataLayer.push(arguments);}
-						gtag('js', new Date());
-					
-						gtag('config', 'G-YTNP4RYFY1');
-					`,
-				},
-				{
-					src: "https://www.googletagmanager.com/gtag/js?id=G-YTNP4RYFY1",
-					async: true,
-				},
-			],
-		},
+	// GOOGLE ANALYTICS
+	gtag: {
+		id: "G-YTNP4RYFY1",
 	},
 	// TAILWIND CSS
 	postcss: {
